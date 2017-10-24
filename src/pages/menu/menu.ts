39c35eb0d9a8178bda890678b6d27a944fa99521
Hermann Caraballo <hcaraballo@dlbgroup.com>
2017-10-24 09:40:49 -0300

    Fix index of tab, now working.
    The index lets the menu choice beetwen pages, if it is the same, pages won't change. Must be ascending and correlative.

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -23,7 +23,7 @@ export class MenuPage {
 
 	pages: PageInterface[] = [
 		{ title: 'Tab 1', pageName: 'TabsPage', tabComponent: 'Tab1Page', index: 0, icon: 'home' },
-		{ title: 'Tab 2', pageName: 'TabsPage', tabComponent: 'Tab2Page', index: 0, icon: 'contacts' },
+		{ title: 'Tab 2', pageName: 'TabsPage', tabComponent: 'Tab2Page', index: 1, icon: 'contacts' },
 		{ title: 'Special', pageName: 'SpecialPage', icon: 'eye' }
 	]
 
